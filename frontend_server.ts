7f90f96662c824e046ed62742d80d4090375963f
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2018-09-12 12:49:23 +0100

    Clean up front-end deployment script

--- frontend_server.ts
+++ frontend_server.ts
@@ -7,15 +7,7 @@ const port = process.env.PORT || 4200;
 
 app.set('port', port);
 app.use(cors());
-// app.use('/SMIS', express.static(path.join(__dirname, 'dist')));
-// app.use('/', express.static(path.join(__dirname, 'dist')));
-
 app.use(express.static(path.join(__dirname, 'dist')));
-
-/*app.get('/', function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'dist/index.html'));
-  });*/
-
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
