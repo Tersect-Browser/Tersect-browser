eabfb2ef8fcfebb28dada6d75cd4eefa21e8b847
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2018-10-03 13:27:24 +0100

    Update base href

--- frontend-server.ts
+++ frontend-server.ts
@@ -7,7 +7,9 @@ const port = process.env.PORT || 4500;
 
 app.set('port', port);
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/TersectBrowser', express.static(path.join(__dirname, 'dist')));
+app.use('*', express.static(path.join(__dirname, 'dist')));
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
