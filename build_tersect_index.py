0e23ac56b614ba9b1505ba0f89a4010083bc5ebd
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-05-13 19:37:36 +0100

    Re-use smaller indices to build larger ones

--- build_tersect_index.py
+++ build_tersect_index.py
@@ -6,8 +6,44 @@ from pymongo import MongoClient
 from math import ceil
 from timeit import default_timer as timer
 
-def add_tersect_index(collection, chromosome_name, start_pos, end_pos,
-                      tersect_db_location, verbose=False):
+# Assumes matrices have the same dimensions (or m1 is None), saves result in m1
+def matrix_sum(m1, m2):
+    if m1 is None:
+        # Initialize empty result matrix
+        m1 = [x[:] for x in [[0] * len(m2[0])] * len(m2)]
+    for i in range(len(m2)):
+        for j in range(len(m2[0])):
+            m1[i][j] += m2[i][j]
+    return m1
+
+def add_region_index_db(collection, chromosome_name, start_pos, end_pos,
+                        subpart_size, verbose=False):
+    region = '%s:%d-%d' % (chromosome_name, start_pos, end_pos)
+    if (verbose):
+        print('    Building index for %s based on existing indices' % region)
+
+    region_matrix = None
+    samples = None
+    for substart_pos in range(start_pos, end_pos + 1, subpart_size):
+        subend_pos = substart_pos + subpart_size - 1
+        subregion = '%s:%d-%d' % (chromosome_name, substart_pos, subend_pos)
+        res = collection.find_one({'region': subregion})
+        if res is not None:
+            region_matrix = matrix_sum(region_matrix, res['matrix'])
+            if samples is None:
+                samples = res['samples']
+        else:
+            # Reached the end of region covered by subpartitions
+            break
+    collection.insert({
+        'region': region,
+        'samples': samples,
+        'matrix': region_matrix
+    })
+    return True
+
+def add_region_index_tersect(collection, chromosome_name, start_pos, end_pos,
+                             tersect_db_location, verbose=False):
     region = '%s:%d-%d' % (chromosome_name, start_pos, end_pos)
     if (verbose):
         print('    Adding index for %s' % region)
@@ -37,13 +73,24 @@ def get_chromosome_sizes(tersect_db_location):
     chroms = [ { 'name': line[0], 'size': int(line[1]) } for line in lines]
     return chroms
 
-def generate_partition_indices(cfg, tindex, chrom, part_size, verbose=False):
+def generate_partition_indices(cfg, tindex, chrom, part_size,
+                               existing_partitions=[], verbose=False):
     if verbose:
         print('  Generating %d-sized partition indices' % part_size)
+
+    partition_divisors = [f for f in existing_partitions if not (part_size % f)]
+
     part_ceil = int(part_size * ceil(float(chrom['size']) / part_size)) + 1
     for pos in range(part_size, part_ceil, part_size):
-        add_tersect_index(tindex, chrom['name'], pos - part_size + 1, pos,
-                          cfg['tsi_location'], verbose)
+        start_pos = pos - part_size + 1
+        if len(partition_divisors):
+            # Build index based on smaller partitions already in the database
+            add_region_index_db(tindex, chrom['name'], start_pos, pos,
+                                max(partition_divisors), verbose)
+        else:
+            # Create index from scratch by calling tersect
+            add_region_index_tersect(tindex, chrom['name'], start_pos, pos,
+                                     cfg['tsi_location'], verbose)
 
 def generate_indices(cfg, verbose=False):
     start = timer()
@@ -55,16 +102,27 @@ def generate_indices(cfg, verbose=False):
     tindex.drop()
     tindex.create_index('region')
 
+    cfg['index_partitions'].sort()
     for chrom in chromosomes:
         if verbose:
             print('Generating indices for %s' % chrom['name'])
-        add_tersect_index(tindex, chrom['name'], 1, chrom['size'],
-                          cfg['tsi_location'], verbose)
-        cfg['index_partitions'].sort()
+        #
+        existing_partitions = []
         for part_size in cfg['index_partitions']:
             if part_size < chrom['size']:
                 generate_partition_indices(cfg, tindex, chrom, part_size,
-                                           verbose)
+                                           existing_partitions, verbose)
+                existing_partitions.append(part_size)
+
+        if verbose:
+            print('Generating whole genome index for %s' % chrom['name'])
+        # Add index for whole chromosome based on largest existing partition
+        if len(existing_partitions):
+            add_region_index_db(tindex, chrom['name'], 1, chrom['size'],
+                                max(existing_partitions), verbose)
+        else:
+            add_region_index_tersect(tindex, chrom['name'], 1, chrom['size'],
+                                     cfg['tsi_location'], verbose)
     client.close()
 
     if verbose:
