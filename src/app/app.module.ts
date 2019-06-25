5099611ee520ae1d9baeba2aca430b6c6a37bf4e
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-06-25 20:19:55 +0100

    Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { IntrogressionBrowserModule } from './introgression-browser/introgression-browser.module';
 
+import { IntrogressionBrowserComponent } from './introgression-browser/introgression-browser.component';
+
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+    { path: '', component: IntrogressionBrowserComponent, pathMatch: 'full' },
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
     declarations: [
@@ -13,7 +21,8 @@ import { AppComponent } from './app.component';
     imports: [
         HttpClientModule,
         BrowserModule,
-        IntrogressionBrowserModule
+        IntrogressionBrowserModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [
         HttpClient
