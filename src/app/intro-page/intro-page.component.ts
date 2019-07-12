bc3f583b527c5152a7feb2f10f5872817f0d569d
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-07-12 23:46:55 +0100

    Load dataset list from database

--- src/app/intro-page/intro-page.component.ts
+++ src/app/intro-page/intro-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TersectBackendService } from '../services/tersect-backend.service';
+import { IDatasetPublic } from '../../backend/db/dataset';
 
 @Component({
     selector: 'app-intro-page',
     templateUrl: './intro-page.component.html',
     styleUrls: ['./intro-page.component.css']
 })
-export class IntroPageComponent {
+export class IntroPageComponent implements OnInit {
+    constructor(private tersectBackendService: TersectBackendService) {}
+
+    datasets: IDatasetPublic[];
+
+    ngOnInit() {
+        this.tersectBackendService.getDatasets().subscribe(ds => {
+            this.datasets = ds;
+            console.log(this.datasets);
+        });
+    }
 }
