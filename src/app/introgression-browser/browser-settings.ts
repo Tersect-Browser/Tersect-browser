fdbd7f7501029f7f1141cb9788e49759beb3eb3d
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-07-12 22:22:09 +0100

    Make all settings except dataset_id optional

--- src/app/introgression-browser/browser-settings.ts
+++ src/app/introgression-browser/browser-settings.ts
@@ -6,11 +6,11 @@ import { AccessionDisplayStyle } from '../services/introgression-plot.service';
  */
 export interface BrowserSettings {
     dataset_id: string;
-    selectedAccessionDisplayStyle: AccessionDisplayStyle;
-    selected_accessions: string[];
-    selected_reference: string;
-    selected_chromosome: Chromosome;
-    selected_interval: number[];
-    selected_binsize: number;
-    zoom_level: number;
+    selectedAccessionDisplayStyle?: AccessionDisplayStyle;
+    selected_accessions?: string[];
+    selected_reference?: string;
+    selected_chromosome?: Chromosome;
+    selected_interval?: number[];
+    selected_binsize?: number;
+    zoom_level?: number;
 }
