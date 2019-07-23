98ef75ee9328bd91cd1520e10b13f098106b03f2
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-07-23 23:17:38 +0100

    Add phylip merge wrappe

---
+++ src/backend/merge_phylip.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import argparse
+from tersectutils import get_accession_names
+from tbutils import merge_phylip_files
+
+def merge_inputs(phylip_files, tsi_file=None, accessions=None):
+    if accessions is None:
+        return merge_phylip_files(args.phylip_files)
+    else:
+        all_accessions = get_accession_names(tsi_file)
+        indices = [ all_accessions.index(acc) for acc in accessions ]
+        indices.sort()
+        return merge_phylip_files(args.phylip_files, indices=indices)
+
+parser = argparse.ArgumentParser(description='Merge specified Phylip files, limited to selected accessions')
+parser.add_argument('tsi_file', type=str, help='tsi file path')
+parser.add_argument('phylip_files', nargs='+', help='Phylip files')
+parser.add_argument('-a', required=False, nargs='+', help='accessions')
+
+args = parser.parse_args()
+
+tmp_merged_file = merge_inputs(args.phylip_files, args.tsi_file, args.a)
+
+print(tmp_merged_file)
--- src/backend/tbutils.py
+++ src/backend/tbutils.py
@@ -21,7 +21,7 @@ def open_phylip_file(location='/tmp', mode='x'):
     except FileExistsError:
         return open_phylip_file(location=location, mode=mode)
 
-def merge_phylip_files(filenames, output_location='/tmp'):
+def merge_phylip_files(filenames, output_location='/tmp', indices=None):
     output_file = open_phylip_file(location=output_location)
     with ExitStack() as stack:
         readers = [
@@ -32,15 +32,30 @@ def merge_phylip_files(filenames, output_location='/tmp'):
 
         # First line is the number of genomes
         genome_num = [int(next(reader)[0]) for reader in readers][0]
-        writer.writerow([genome_num])
 
-        for lines in zip(*readers):
-            arrays = [
-                numpy.asarray(list(map(int, line[1:])))
-                for line in lines
-            ]
-            output_file.write(lines[0][0] + ' ')
-            writer.writerow(sum(arrays))
+        if indices is None:
+            # Merging all accessions
+            writer.writerow([genome_num])
+            for lines in zip(*readers):
+                arrays = [
+                    numpy.asarray(list(map(int,line[1:])))
+                    for line in lines
+                ]
+                output_file.write(lines[0][0] + ' ')
+                writer.writerow(sum(arrays))
+        else:
+            # Merging only accessions at specified indices
+            writer.writerow([len(indices)])
+            for index, lines in enumerate(zip(*readers)):
+                if index not in indices:
+                    continue
+                arrays = [
+                    numpy.asarray(list(map(int,
+                        [ line[1:][i] for i in indices ])
+                    )) for line in lines
+                ]
+                output_file.write(lines[0][0] + ' ')
+                writer.writerow(sum(arrays))
 
     output_file.close()
     return output_file.name
