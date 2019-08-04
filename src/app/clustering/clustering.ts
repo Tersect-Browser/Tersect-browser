4a66230d9ffd22b14a17bb44694da173bbc49288
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-08-04 02:11:33 +0100

    Remove unused clustering dependency

--- src/app/clustering/clustering.ts
+++ src/app/clustering/clustering.ts
@@ -1,7 +1,4 @@
-import { DistanceMatrix } from '../models/DistanceMatrix';
-
 import { isNullOrUndefined } from 'util';
-import * as nj from 'neighbor-joining';
 import * as newick_parser from 'biojs-io-newick';
 
 export type TreeNode = {
@@ -67,33 +64,6 @@ function _getTreeDepth(subtree: TreeNode, depth: number,
             output.max_depth = depth;
         }
     }
-}
-
-/**
- * Build a neighbor-joining tree.
- */
-export function buildNJTree(distance_matrix: DistanceMatrix,
-                            accessions_to_plot: string[]): TreeNode {
-    // Find indices of accessions to plot
-    const plotted_indices = accessions_to_plot.map(acc => {
-        return distance_matrix.samples.indexOf(acc);
-    });
-
-    const filtered_matrices = plotted_indices.map(ind => {
-        return plotted_indices.map(inner_ind => {
-            return distance_matrix.matrix[ind][inner_ind];
-        });
-    });
-    const filtered_accession_names = plotted_indices.map(ind => {
-        return distance_matrix.samples[ind];
-    });
-
-    const accessions = filtered_accession_names.map((x) => {
-        return { name: x };
-    });
-    const RNJ = new nj.RapidNeighborJoining(filtered_matrices, accessions);
-    RNJ.run();
-    return RNJ.getAsObject();
 }
 
 /**
