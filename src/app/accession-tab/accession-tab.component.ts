8317729e5cd54590c7a24b16e84a0072f4357804
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-08-09 19:22:31 +0100

    Virtual table prototype WIP

--- src/app/accession-tab/accession-tab.component.ts
+++ src/app/accession-tab/accession-tab.component.ts
@@ -1,17 +1,18 @@
 import { PlotStateService } from '../introgression-plot/services/plot-state.service';
 
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 interface AccessionRow {
     id?: string;
-};
+}
 
 @Component({
     selector: 'app-accession-tab',
     templateUrl: './accession-tab.component.html',
-    styleUrls: ['./accession-tab.component.css']
+    styleUrls: ['./accession-tab.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
-export class AccessionTabComponent {
+export class AccessionTabComponent implements OnInit {
     @Output() updateAccessions = new EventEmitter<string[]>();
 
     @Input()
@@ -19,18 +20,27 @@ export class AccessionTabComponent {
 
     cols: any[];
 
-    accessions_rows: AccessionRow[];
+    accession_rows: AccessionRow[];
+    accession_count: number;
+    virtual_accession_rows: AccessionRow[];
 
     constructor(private plotState: PlotStateService) {}
 
     ngOnInit() {
-        this.accessions_rows = this.plotState.accessions.map((acc_id) => {
+        this.accession_rows = this.plotState.accessions.map((acc_id) => {
             return {
                 id: acc_id
             };
-        })
+        });
+        this.accession_count = this.accession_rows.length;
+        this.virtual_accession_rows = this.accession_rows.slice(0, 40);
         this.cols = [
             { field: 'id', header: 'ID' }
         ];
+    }
+
+    loadDataOnScroll($event) {
+        console.log($event);
+        this.virtual_accession_rows = this.accession_rows.slice(0, 40);
     }
 }
