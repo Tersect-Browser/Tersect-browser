6407970fdb34d9bcf331bf830afa450796e609c5
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-08-13 22:15:45 +0100

    Complete filter prototype

--- src/app/accession-tab/accession-tab.component.ts
+++ src/app/accession-tab/accession-tab.component.ts
@@ -1,7 +1,8 @@
+import { sameElements } from '../utils/utils';
+import { TableState } from 'primeng/components/common/tablestate';
+
 import { Component, Output, EventEmitter, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { isNullOrUndefined } from 'util';
-
-import { TableState } from 'primeng/components/common/tablestate';
 
 export interface AccessionRow {
     id?: string;
@@ -21,10 +22,7 @@ export class AccessionTabComponent implements OnInit {
     @Input()
     set selectedAccessions(accessions: string[]) {
         this._selectedAccessions = accessions;
-        if (!isNullOrUndefined(this.accession_options)) {
-            this.all_selected = this.accession_options.length
-                                === this.selectedAccessions.length;
-        }
+        this.updateAllSelected();
         this.selectedAccessionsChange.emit(this._selectedAccessions);
     }
     get selectedAccessions(): string[] {
@@ -56,10 +54,31 @@ export class AccessionTabComponent implements OnInit {
 
     headerCheckboxChange($event: boolean) {
         if ($event) {
-            this.selectedAccessions = this.accession_options.map(acc => acc.id);
+            this.filtered_accessions.forEach(acc => {
+                if (!this.selectedAccessions.includes(acc.id)) {
+                    this.selectedAccessions.push(acc.id);
+                }
+            });
         } else {
-            this.selectedAccessions = [];
+            // Unchecking all matching filters
+            const filtered_ids = this.filtered_accessions.map(acc => acc.id);
+            this.selectedAccessions = this.selectedAccessions.filter(acc => {
+                return !filtered_ids.includes(acc);
+            });
         }
+    }
+
+    updateAllSelected() {
+        if (!isNullOrUndefined(this.filtered_accessions)) {
+            this.all_selected = this.filtered_accessions.every(
+                acc => this.selectedAccessions.includes(acc.id)
+            );
+        }
+    }
+
+    filtersUsed(): boolean {
+        return this.filtered_accessions.length
+               !== this.accession_options.length;
     }
 
     loadDataOnScroll($event: TableState) {
@@ -73,5 +92,6 @@ export class AccessionTabComponent implements OnInit {
         this.virtual_accession_rows = this.filtered_accessions
                                           .slice($event.first,
                                                  $event.first + $event.rows);
+        this.updateAllSelected();
     }
 }
