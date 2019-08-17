aae12202924a83c7f538f7cd2a12de316b186bba
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-08-17 20:24:58 +0100

    Add directive for fitting fixed elements in window

---
+++ src/app/directives/fit-window/fit-window.directive.ts
@@ -0,0 +1,57 @@
+import { Directive, ElementRef, Input } from "@angular/core";
+
+@Directive({
+    selector: '[appFitWindow]'
+})
+export class FitWindowDirective {
+    @Input()
+    cursorSize = 20;
+
+    private observer = new MutationObserver(() => { this.adjustPosition(); });
+
+    private get position(): { x: number, y: number } {
+        const left_px = this.el.nativeElement.style.left;
+        const top_px = this.el.nativeElement.style.top;
+        return {
+            x: parseInt(left_px.substring(0, left_px.length - 2)),
+            y: parseInt(top_px.substring(0, top_px.length - 2))
+        };
+    }
+
+    private set position(pos: { x: number, y: number }) {
+        this.el.nativeElement.style.left = `${pos.x}px`;
+        this.el.nativeElement.style.top = `${pos.y}px`;
+    }
+
+    constructor(private el: ElementRef) {
+        this.observer.observe(this.el.nativeElement, { attributes: true });
+    }
+
+    /**
+     * Adjusts menu position so that is does not overflow.
+     */
+    private adjustPosition() {
+        const width = this.el.nativeElement.offsetWidth;
+        const height = this.el.nativeElement.offsetHeight;
+
+        if (width > window.innerWidth
+            || height > window.innerHeight) {
+            // No way to fit the menu inside the plot area
+            return;
+        }
+
+        const pos = this.position;
+        const x_overflow = pos.x + width - window.innerWidth;
+        const y_overflow = pos.y + height - window.innerHeight;
+
+        if (x_overflow > 0 || y_overflow > 0) {
+            this.position = {
+                x: x_overflow > 0 ? pos.x - x_overflow
+                                  : pos.x,
+                y: y_overflow > 0 ? pos.y - height - this.cursorSize
+                                  : pos.y
+            };
+            this.el.nativeElement.style.visibility = 'visible';
+        }
+    }
+}
---
+++ src/app/directives/fit-window/fit-window.module.ts
@@ -0,0 +1,13 @@
+import { FitWindowDirective } from "./fit-window.directive";
+
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [
+        FitWindowDirective
+    ],
+    exports: [
+        FitWindowDirective
+    ]
+})
+export class FitWindowModule { }
