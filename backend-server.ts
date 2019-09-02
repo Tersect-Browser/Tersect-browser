5af9036d75bbebd4b1ee43da97f0aeebd9d4b9e5
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-09-02 17:41:32 +0100

    Build back-end URI based on config file

--- backend-server.ts
+++ backend-server.ts
@@ -1,6 +1,7 @@
 import express = require('express');
 import mongoose = require('mongoose');
 import bodyParser = require('body-parser');
+import * as fs from 'fs';
 
 import { router } from './src/backend/tersect-router';
 import { cleanDatabase } from './src/backend/db/dbutils';
@@ -8,7 +9,12 @@ import { cleanDatabase } from './src/backend/db/dbutils';
 const app = express();
 const port = process.env.PORT || 8060;
 
-mongoose.connect('mongodb://localhost:27017/tersect', { useNewUrlParser: true });
+const config = JSON.parse(
+    fs.readFileSync('./src/backend/config.json').toString()
+);
+
+const uri = `${config.mongo_hostname}:${config.port}/${config.db_name}`;
+mongoose.connect(uri, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
 app.use(bodyParser.json({ limit: '1mb' }));
