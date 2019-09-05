5a903c587500966d9ac16476bd50228cbc2cc750
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-09-05 10:54:24 +0100

    Fix mongoose typing

--- src/backend/db/dbutils.ts
+++ src/backend/db/dbutils.ts
@@ -2,11 +2,12 @@ import { PheneticTree } from './phenetictree';
 
 export async function cleanDatabase() {
     // Clean up unfinished trees.
-    await PheneticTree.deleteMany({ 'status': { $ne: 'ready' }}, (err, out) => {
+    await PheneticTree.deleteMany({ 'status': { $ne: 'ready' }}, err => {
         if (err) {
             console.log(err);
             process.exit(0);
         }
+    }).then(out => {
         if (out.deletedCount) {
             console.log(`Removed ${out.deletedCount} incomplete tree(s).`);
         }
