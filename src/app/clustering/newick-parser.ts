42d9087738f708a1a4bcb3819f128c5487dd238f
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-09-12 17:42:48 +0100

    Reorder functions

--- src/app/clustering/newick-parser.ts
+++ src/app/clustering/newick-parser.ts
@@ -1,6 +1,11 @@
 import { isNullOrUndefined, syncSort } from '../utils/utils';
 import { TreeNode } from './clustering';
 
+export function ladderizeTree(tree: TreeNode, weighted = false): TreeNode {
+    _ladderizeSubtree(tree, weighted);
+    return tree;
+}
+
 /**
  * Returns number of taxons in subtree (if weighted is false - the default)
  * or sum of lengths (distances) inside the subtree (if weighted is true)
@@ -17,18 +22,10 @@ function _ladderizeSubtree(node: TreeNode, weighted = false): number {
     }
 }
 
-export function ladderizeTree(tree: TreeNode, weighted = false): TreeNode {
-    _ladderizeSubtree(tree, weighted);
-    return tree;
-}
-
-function* newickTokens(newick: string, stripQuotes = true): Iterator<string> {
-    const tokens = newick.split(/\s*(,|:|;|\(|\))\s*/);
-    for (const token of tokens) {
-        if (token === '') { continue; }
-        if (token === ';') { continue; }
-        stripQuotes ? yield token.replace(/^'+|'+$/g, '') : yield token;
-    }
+export function parseNewick(newick: string, ladderize = false) {
+    const tokens = newickTokens(newick);
+    const tree = _parseNewick(tokens);
+    return ladderize ? ladderizeTree(tree, true) : tree;
 }
 
 function _parseNewick(tokens: Iterator<string>,
@@ -63,8 +60,11 @@ function _parseNewick(tokens: Iterator<string>,
     return output;
 }
 
-export function parseNewick(newick: string, ladderize = false) {
-    const tokens = newickTokens(newick);
-    const tree = _parseNewick(tokens);
-    return ladderize ? ladderizeTree(tree, true) : tree;
+function* newickTokens(newick: string, stripQuotes = true): Iterator<string> {
+    const tokens = newick.split(/\s*(,|:|;|\(|\))\s*/);
+    for (const token of tokens) {
+        if (token === '') { continue; }
+        if (token === ';') { continue; }
+        stripQuotes ? yield token.replace(/^'+|'+$/g, '') : yield token;
+    }
 }
