2a526ae09bd595455e2a16e02b28afe2c17853b3
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-11-05 22:00:41 +0000

    Load proxy port dynamically

--- frontend/src/proxy.conf.js
+++ frontend/src/proxy.conf.js
@@ -1,10 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const configFile = path.join(__dirname, '../../tbconfig.json');
+const tbConfig = JSON.parse(fs.readFileSync(configFile).toString());
+const devPort = process.env.PORT
+                || tbConfig.server_port_dev
+                || tbConfig.server_port;
+
 const PROXY_CONFIG = [
     {
         context: [
-            "/TersectBrowser/tbapi",
-            "/TersectBrowser/tgrc"
+            '/TersectBrowser/tbapi',
+            '/TersectBrowser/tgrc'
         ],
-        target: "http://localhost:4500",
+        target: 'http://localhost:' + devPort,
         secure: false
     }
 ];
