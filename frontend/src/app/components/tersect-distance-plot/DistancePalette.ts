144e84c55d8eaee137b678820fa1d3d803a99310
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2019-11-28 12:11:28 +0000

    Allow different color spaces in interpolation

--- frontend/src/app/components/tersect-distance-plot/DistancePalette.ts
+++ frontend/src/app/components/tersect-distance-plot/DistancePalette.ts
@@ -1,3 +1,5 @@
+import * as convert from 'color-convert';
+
 export abstract class DistancePalette {
     protected readonly palette: ImageData[];
 
@@ -19,20 +21,52 @@ export abstract class DistancePalette {
     }
 }
 
-export class InterpolatedPalette extends DistancePalette {
-    constructor(startColor: number[],
-                endColor: number[],
-                levels = 256) {
-        super();
-        for (let i = 0; i < levels; i++) {
-            const endWeight = i / (levels - 1);
+type ColorSpace = 'rgb' | 'hsv' | 'hsl' | 'hwb';
 
-            this.palette[i] = new ImageData(1, 1);
-            for (let c = 0; c < 4; c++) {
-                this.palette[i].data[c] = Math.round(
+interface InterpolationOptions {
+    inputColorSpace?: ColorSpace;
+    colorSpace?: ColorSpace;
+    levels?: number;
+}
+
+class InterpolatedPalette extends DistancePalette {
+    private static readonly DEFAULT_COLOR_SPACE = 'rgb';
+    private static readonly DEFAULT_LEVEL_NUM = 256;
+
+    constructor(inputStartColor: number[],
+                inputEndColor: number[],
+                options: InterpolationOptions = {
+                    inputColorSpace: InterpolatedPalette.DEFAULT_COLOR_SPACE,
+                    colorSpace: InterpolatedPalette.DEFAULT_COLOR_SPACE,
+                    levels: InterpolatedPalette.DEFAULT_LEVEL_NUM
+                }) {
+        super();
+        options.inputColorSpace = options.inputColorSpace
+                                  || InterpolatedPalette.DEFAULT_COLOR_SPACE;
+        options.colorSpace      = options.colorSpace
+                                  || InterpolatedPalette.DEFAULT_COLOR_SPACE;
+        options.levels          = options.levels
+                                  || InterpolatedPalette.DEFAULT_LEVEL_NUM;
+
+        const startColor = convertColorSpace(inputStartColor,
+                                             options.inputColorSpace,
+                                             options.colorSpace);
+        const endColor = convertColorSpace(inputEndColor,
+                                           options.inputColorSpace,
+                                           options.colorSpace);
+
+        for (let i = 0; i < options.levels; i++) {
+            const endWeight = i / (options.levels - 1);
+            let color = [];
+            for (let c = 0; c < 3; c++) {
+                color[c] = Math.round(
                     startColor[c] + (endColor[c] - startColor[c]) * endWeight
                 );
             }
+            color = convertColorSpace(color, options.colorSpace, 'rgb');
+            color[3] = 255; // No alpha interpolation
+            this.palette[i] = new ImageData(1, 1);
+            this.palette[i].data.set(color);
         }
     }
 
@@ -41,8 +75,20 @@ export class InterpolatedPalette extends DistancePalette {
     }
 }
 
-export const GreyscalePalette = new InterpolatedPalette([255, 255, 255, 255],
-                                                        [0, 0, 0, 255]);
+export const GreyscalePalette = new InterpolatedPalette([255, 255, 255],
+                                                        [0, 0, 0]);
 
-export const RedPalette = new InterpolatedPalette([255, 255, 255, 255],
-                                                  [255, 0, 0, 255]);
+export const RedPalette = new InterpolatedPalette([255, 255, 255],
+                                                  [255, 0, 0]);
+
+export const BlueToRedPalette = new InterpolatedPalette([0, 0, 255],
+                                                        [255, 0, 0],
+                                                        { colorSpace: 'hwb' });
+
+function convertColorSpace(color: number[], from: ColorSpace, to: ColorSpace) {
+    if (from === to) {
+        return color;
+    } else {
+        return convert[from][to](color);
+    }
+}
