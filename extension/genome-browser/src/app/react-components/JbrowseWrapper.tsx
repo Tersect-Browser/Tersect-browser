2cc4367268718dbd5f2b337b8292d6508016b3f1
David Oluwasusi <daviddamilola20@gmail.com>
2025-03-25 18:22:22 +0000

    style tersect browser

--- extension/genome-browser/src/app/react-components/JbrowseWrapper.tsx
+++ extension/genome-browser/src/app/react-components/JbrowseWrapper.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   createViewState,
   JBrowseLinearGenomeView,
+  ViewModel
 } from '@jbrowse/react-linear-genome-view'
 import assembly from './assembly';
 import tracks from './tracks';
@@ -9,13 +10,33 @@ import tracks from './tracks';
 
 
 
-function JbrowserWrapper({ name }: { name: string }) {
+function JbrowserWrapper(props: any) {
+
     const state = createViewState({
         assembly,
         tracks,
         location: '1:100,987,269..100,987,368',
+        configuration: {
+          "theme": {
+            "palette": {
+              "primary": {
+                "main": "#459e00"
+              },
+              "secondary": {
+                "main": "#459e00"
+              },
+              "tertiary": {
+                "main": "#459e00"
+              },
+              "quaternary": {
+                "main": "#459e00"
+              }
+            }
+      
+          }
+        }
       })
-  return <JBrowseLinearGenomeView viewState={state} />
+  return <JBrowseLinearGenomeView   viewState={state} />
 }
 
 export default JbrowserWrapper
