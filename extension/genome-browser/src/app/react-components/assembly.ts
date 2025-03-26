a7900ee92e584c0120c8c60e5543d2b03bc0fb09
tanyastead <tatiana.stead@outlook.com>
2025-03-26 10:55:16 +0000

    Updated tracks for all accessions and full path for backend loading

--- extension/genome-browser/src/app/react-components/assembly.ts
+++ extension/genome-browser/src/app/react-components/assembly.ts
@@ -1,63 +1,19 @@
 export default {
-    /*name: 'GRCh38',
-    sequence: {
-      type: 'ReferenceSequenceTrack',
-      trackId: 'GRCh38-ReferenceSequenceTrack',
-      adapter: {
-        type: 'BgzipFastaAdapter',
-        fastaLocation: {
-          uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz',
-          locationType: 'UriLocation',
+    "name": "SL2.50",
+    "sequence": {
+      "type": "ReferenceSequenceTrack",
+      "trackId": "SL2.50-ReferenceSequenceTrack",
+      "adapter": {
+        "type": "IndexedFastaAdapter",
+        "fastaLocation": {
+          "uri": "http://localhost:4200/TersectBrowserGP/tbapi/datafiles//SL2.50.fa",
+          "locationType": "UriLocation"
         },
-        faiLocation: {
-          uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.fai',
-          locationType: 'UriLocation',
-        },
-        gziLocation: {
-          uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.gzi',
-          locationType: 'UriLocation',
-        },
-      },
-    },
-    aliases: ['hg38'],
-    refNameAliases: {
-      adapter: {
-        type: 'RefNameAliasAdapter',
-        location: {
-          uri: 'http://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/GRCh38.aliases.txt',
-          locationType: 'UriLocation',
-        },
-      },
-    }, */
-    
-      /*"assembly": [ 
-        { */
-                   "name": "SL2.50",
-                    "sequence": {
-                      "type": "ReferenceSequenceTrack",
-                      "trackId": "SL2.50-ReferenceSequenceTrack",
-                      "adapter": {
-                        "type": "IndexedFastaAdapter",
-                        "fastaLocation": {
-                          "uri": "http://localhost:8000/SL2.50.fa",
-                          "locationType": "UriLocation"
-                        },
-                        "faiLocation": {
-                          "uri": "http://localhost:8000/SL2.50.fa.fai",
-                          "locationType": "UriLocation"
-                        }
-                      }
-                    }
-        /*}
-      ],*/
-      /*
-      "configuration": {},
-      "connections": [],
-      "defaultSession": {
-        "name": "New Session"
-      },
-      "tracks": []
-      */
-    
+        "faiLocation": {
+          "uri": "http://localhost:4200/TersectBrowserGP/tbapi/datafiles//SL2.50.fa.fai",
+          "locationType": "UriLocation"
+        }
+      }
+    }
   }
   
