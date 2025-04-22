e370de7f297639d6c4efe47ad71b4264ff5c6ffe
Gabrielle Baumberg <gabrielle.baumberg.757@cranfield.ac.uk>
2025-04-22 16:37:24 +0100

    Tersect index requires unzipped vcf files as workaround for legacy zcat errors

--- add_example_dataset.sh
+++ add_example_dataset.sh
@@ -3,10 +3,10 @@
 
 SCRIPT="./backend/src/scripts/add_dataset.py"
 CONFIG_FILE="./tbconfig.json"
-DATASET_NAME="Example dataset"
-TSI_FILE="./gp_data/SGN_aer_hom_snps.tsi"
-REFERENCE_NAME="SL2.50"
-REFERENCE="./gp_data/SL2.50.fa"
+DATASET_NAME="data"
+TSI_FILE="./data.tsi"  
+REFERENCE_NAME="GCF_000004515.6_Glycine_max_v4.0_genomic.fa.gz"
+REFERENCE="./GCF_000004515.6_Glycine_max_v4.0_genomic.fa.gz"
 
 $SCRIPT $CONFIG_FILE -f \
 "$DATASET_NAME" "$TSI_FILE" \
