617bb3c39fb518a680a8e42d9afe5fcf7a34941b
David Oluwasusi <daviddamilola20@gmail.com>
2025-05-09 09:19:15 +0100

    restore tracks

--- add_example_dataset.sh
+++ add_example_dataset.sh
@@ -4,9 +4,9 @@
 SCRIPT="./backend/src/scripts/add_dataset.py"
 CONFIG_FILE="./tbconfig.json"
 DATASET_NAME="SGN_aer_hom_snps"
-TSI_FILE="/Users/davidoluwasusi/msc_project/tersect-browser/test-data/mongo-data/gp_data_copy//SGN_aer_hom_snps.tsi"  
-REFERENCE_NAME="test-data/mongo-data/gp_data_copy/SL2.50.fa"
-REFERENCE="./test-data/mongo-data/gp_data_copy/SL2.50.fa"
+TSI_FILE="/Users/davidoluwasusi/msc_project/tersect-browser/db-data/mongo-data/gp_data_copy/SGN_aer_hom_snps.tsi"  
+REFERENCE_NAME="db-data/mongo-data/gp_data_copy/SL2.50.fa"
+REFERENCE="./db-data/mongo-data/gp_data_copy/SL2.50.fa"
 
 $SCRIPT $CONFIG_FILE -f \
 "$DATASET_NAME" "$TSI_FILE" \
